e04faa6d2025819ffc20c68993a9fffb185b09f8
bablu kumar <bablu707041dk@gmail.com>
2026-01-11 23:16:11 +0530

    Added CategoryRequest and CategoryResponse DTOs

---
+++ src/main/java/in/bablu/blooms/dto/CategoryRequest.java
@@ -0,0 +1,46 @@
+package in.bablu.blooms.dto;
+
+// DTO (Data Transfer Object) - Ye wo parchhi hai jo User bharke dega.
+// Isme sirf wo data hota hai jo User se server tak aana chahiye.
+public class CategoryRequest {
+    private String id;      // Update karte waqt ID chahiye hogi
+    private String title;   // Category ka naam (e.g. "Tech")
+    private String desc;    // Description
+    private String cUrl; // Image URL (Short variable name use kiya hai sir ne)
+
+    // Default Constructor (Frameworks ko kabhi-kabhi khali constructor chahiye hota hai)
+    public CategoryRequest(){
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getcUrl() {
+        return cUrl;
+    }
+
+    public void setcUrl(String cUrl) {
+        this.cUrl = cUrl;
+    }
+}
---
+++ src/main/java/in/bablu/blooms/dto/CategoryResponse.java
@@ -0,0 +1,59 @@
+package in.bablu.blooms.dto;
+
+// Ye wo packet hai jo Server se Client (UI) ke paas jayega.
+// Hum Model (Category) ko direct nahi bhejte, balki is wrapper me daal kar bhejte hain.
+public class CategoryResponse {
+
+    private String id;      // Frontend ko ID chahiye taaki wo edit/delete button bana sake
+    private String title;   // Name
+    private String desc;    // Description
+    private String cUrl;    // Image URL
+
+    // 1. Default Constructor
+    // (Zaruri hota hai jab frameworks JSON se Object banate hain)
+    public CategoryResponse() {
+    }
+
+    // 2. Parameterized Constructor
+    // Taaki hum ek line me pura object bana sakein: new CategoryResponse(id, t, d, u)
+    public CategoryResponse(String id, String title, String desc, String cUrl) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.cUrl = cUrl;
+    }
+    // --- Getters and Setters ---
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getcUrl() {
+        return cUrl;
+    }
+
+    public void setcUrl(String cUrl) {
+        this.cUrl = cUrl;
+    }
+}
