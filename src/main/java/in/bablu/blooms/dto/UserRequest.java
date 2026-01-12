40ef002d54ef1d71011a5f9e9d746dfb53635c4f
bablu kumar <bablu707041dk@gmail.com>
2026-01-12 21:06:58 +0530

    Created The UserRequest

---
+++ src/main/java/in/bablu/blooms/dto/UserRequest.java
@@ -0,0 +1,69 @@
+package in.bablu.blooms.dto;
+
+public class UserRequest {
+    private String id;
+    private String username;
+    private String email;
+    private String name;
+    private String password;
+    private String profileUrl;
+
+    public UserRequest(){
+
+    }
+    public UserRequest(String username, String email, String name, String password, String profileUrl) {
+        this.username = username;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.profileUrl = profileUrl;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getProfileUrl() {
+        return profileUrl;
+    }
+
+    public void setProfileUrl(String profileUrl) {
+        this.profileUrl = profileUrl;
+    }
+}
