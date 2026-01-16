c537b5279812cba940726be6835d726e6154426b
bablu kumar <bablu707041dk@gmail.com>
2026-01-17 01:21:30 +0530

    config: add MongoConfig to force connection with MongoDB Atlas

--- src/main/java/in/bablu/blooms/BloomsApplication.java
+++ src/main/java/in/bablu/blooms/BloomsApplication.java
@@ -2,12 +2,18 @@ package in.bablu.blooms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class BloomsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BloomsApplication.class, args);
-	}
+    public static void main(String[] args) {
 
+        ConfigurableApplicationContext context = SpringApplication.run(BloomsApplication.class, args);
+
+        String mongoUri = context.getEnvironment().getProperty("spring.data.mongodb.uri");
+        System.out.println("--------------------------------------------------");
+        System.out.println("🔥 MY MONGO URI IS: " + mongoUri);
+        System.out.println("--------------------------------------------------");
+    }
 }
